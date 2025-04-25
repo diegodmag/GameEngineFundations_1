339b6c40a0cc723de79e2fd82efdecdbc9b730e9
diegodmag <diegodm@ciencias.unam.mx>
2025-04-25 11:29:16 -0600

    Se agrega Makefile y Matrix3D.cpp

--- scripts/main.cpp
+++ scripts/main.cpp
@@ -1,31 +1,21 @@
 #include <iostream>
-//#include "Vector3D.h"
+#include "Vector3D.h"
 #include "Matrix3D.h"
 
 using namespace std; 
 
 int main(){
 
-    // Vector3D v(1.0f, 2.0f, 3.0f); 
+    Vector3D a(1.0f, 2.0f, 3.0f); 
 
-    // Vector3D v_2(4.0f, 2.0f, 8.0f); 
-    
-    // // v*=3; 
+    Vector3D b = a*2; 
 
-    // // v/=2; 
+    Vector3D c = a*3; 
 
-    // Vector3D v_n = Normalize(v); 
+    Matrix3D m(a,b,c); 
 
-    // cout <<  Magnitude(v_n) << endl; 
+    cout<<m.toString()<<endl; 
 
-    // cout << (v + v_2).toString() << endl; 
-
-    Matrix3D m(1.0f, 2.0f, 3.0f,
-            4.0f, 5.0f, 3.0f,
-            7.0f, 8.0f, 9.0f); 
-
-    cout << m.toString() << endl; 
-
-/* code */
-return 0;
+    /* code */
+    return 0;
 }
